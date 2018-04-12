f52fea2b2d33ee353256dbbf3faf498e6ba05e72
ChenHuan <chscls@163.com>
2018-04-12 17:02:44 +0800

    111111111

--- src/components/SiderMenu/SiderMenu.js
+++ src/components/SiderMenu/SiderMenu.js
@@ -215,7 +215,7 @@ export default class SiderMenu extends PureComponent {
         <div className={styles.logo} key="logo">
           <Link to="/">
             <img src={logo} alt="logo" />
-            <h1>答题宝</h1>
+            <h1>智兔®</h1>
           </Link>
         </div>
         <Menu
