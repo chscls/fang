78f7ecf49422925092006f70da09ff79b0f06348
ChenHuan <chscls@163.com>
2018-04-13 15:16:10 +0800

    111111111

--- src/layouts/BasicLayout.js
+++ src/layouts/BasicLayout.js
@@ -111,9 +111,9 @@ class BasicLayout extends React.PureComponent {
   getPageTitle() {
     const { routerData, location } = this.props;
     const { pathname } = location;
-    let title = 'Ant Design Pro';
+    let title = '智兔';
     if (routerData[pathname] && routerData[pathname].name) {
-      title = `${routerData[pathname].name} - Ant Design Pro`;
+      title = `${routerData[pathname].name} - 智兔`;
     }
     return title;
   }
