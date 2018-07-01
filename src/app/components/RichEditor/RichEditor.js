d109e8ee609840dad0b32b41aad28e269a3e621d
chscls@163.com <chscls@163.com>
2018-07-01 20:03:38 +0800

    1111111

--- src/app/components/RichEditor/RichEditor.js
+++ src/app/components/RichEditor/RichEditor.js
@@ -2,21 +2,9 @@ import React, { PureComponent } from 'react';
 
 import ReactQuill from 'react-quill';
 import theme from 'react-quill/dist/quill.snow.css';
-
-import { Modal, Button, Input, Icon, Upload, message } from 'antd';
-const props = {
-  name: 'file',
-  showUploadList: 'false',
-  customRequest: function(file) {
-    var reader = new FileReader();
-    reader.readAsDataURL(file.file);
-    reader.onload = function(e) {
-      alert(this.result);
-    };
-  },
-};
-
-const InputGroup = Input.Group;
+import config from '../../config';
+import { Modal, Button, message, Icon, Upload } from 'antd';
+const uploadUrl = config.httpServer + '/services/PublicSvc/upload';
 /*
  * Event handler to be attached using Quill toolbar module
  * http://quilljs.com/docs/modules/toolbar/
@@ -25,6 +13,17 @@ const InputGroup = Input.Group;
 /*
  * Custom toolbar component including insertStar button and dropdowns
  */
+function beforeUpload(file) {
+  const isJPG = file.type === 'image/jpeg'||'image/png';
+  if (!isJPG) {
+    message.error('Y请上传jpg或者png格式图片!');
+  }
+  const isLt2M = file.size / 1024 / 1024 < 1;
+  if (!isLt2M) {
+    message.error('请上传小于1MB的图片!');
+  }
+  return isJPG && isLt2M;
+}
 
 export default class RichEditor extends PureComponent {
   constructor(props) {
@@ -39,32 +38,61 @@ export default class RichEditor extends PureComponent {
     };
 
     this.state = {
-      editorHtml: this.props.defaultValue == null ? '' : this.props.defaultValue,
+      editorHtml: this.props.defaultValue == null ? '' : this.props.defaultValue.replace("<img src=\"","<img src=\""+config.httpServer),
     };
   }
-  handleChange(content, delta, source, editor) {
+  handleChange(content) {
     this.setState({ editorHtml: content });
     if (this.props.onChangeValue) {
-      this.props.onChangeValue(content);
+      this.props.onChangeValue(content.replace(config.httpServer,""));
     }
   }
 
   render() {
+    const props = {
+      name: 'file',
+      action: uploadUrl,
+      showUploadList:false,
+      beforeUpload:beforeUpload,
+      headers: {
+        authorization: 'authorization-text',
+      },
+      onChange:(info)=> {
+        if (info.file.status !== 'uploading') {
+          console.log(info.file, info.fileList);
+        }
+        if (info.file.status === 'done') {
+          message.success(`${info.file.name} file uploaded successfully`);
+    
+          var x =  '<img src=\''+config.httpServer+info.file.response+'\'/>'
+            this.setState({ editorHtml: this.state.editorHtml+x})
+           
+          
+          
+        } else if (info.file.status === 'error') {
+          message.error(`${info.file.name} file upload failed.`);
+        }
+      },
+    };
+    
     return (
       <div className={this.props.className} style={this.props.style}>
-        <div id={this.id}>
-          <select className="ql-header" defaultValue={''} onChange={e => e.persist()}>
+        <div id={this.id} style={{ display: "flex" }}>
+          <div> <select className="ql-header" defaultValue={''} onChange={e => e.persist()}>
             <option value="1" />
             <option value="2" />
             <option selected />
-          </select>
-          <button title="粗体" className="ql-bold" />
+          </select></div>
 
-          <button title="斜体" className="ql-italic" />
-          <button title="下划线" className="ql-underline" />
+          <div> <button title="粗体" className="ql-bold" /></div>
 
-          <button title="上传图片" className="ql-image" />
-          <select className="ql-color">
+          <div> <button title="斜体" className="ql-italic" /></div>
+          <div> <button title="下划线" className="ql-underline" /></div>
+          <Upload style={{ cursor:"pointer" }}{...props}>
+            <Icon type="picture" />
+          </Upload>
+
+          <div><select className="ql-color">
             <option value="red" />
             <option value="green" />
             <option value="blue" />
@@ -72,10 +100,10 @@ export default class RichEditor extends PureComponent {
             <option value="violet" />
             <option value="#d0d1d2" />
             <option selected />
-          </select>
-          <button title="公式">
+          </select></div>
+          <div><button title="公式">
             <Icon type="api" />
-          </button>
+          </button></div>
         </div>
 
         <ReactQuill
