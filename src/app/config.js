8de57a3674fe2f5de355349a6ac60a04624c8efe
chscls@163.com <chscls@163.com>
2018-07-02 16:42:28 +0800

    1111111

--- src/app/config.js
+++ src/app/config.js
@@ -1,7 +1,7 @@
 const config = {
   oauthServer: 'http://192.168.1.220:5000',
   httpServer: 'https://www.zymoocs.com/ptd',
-  webServer: 'http://localhost:8000',
+  webServer: 'http://www.zhitail.com',
   socketServer: 'ws://www.zymoocs.com:52000/qcodeWebSocket',
 };
 
